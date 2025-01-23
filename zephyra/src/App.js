b57b3b306a4c03a334b3d542de36fe92b8567ebd
Abhinav Jain <jainabhinav2k16@gmail.com>
2025-01-24 04:57:41 +0530

    refactor: Use Redux for state management instead of local state
    - Integrated Redux for managing the state of uploaded files
    - Added filesSlice for handling file-related actions and state
    - Updated FileUpload component to dispatch actions to Redux store
    - Updated FileGrid component to read files from Redux store
    - Configured Redux store with middleware to handle non-serializable values

--- zephyra/src/App.js
+++ zephyra/src/App.js
@@ -1,17 +1,12 @@
 import FileGrid from "FileGrid/FileGrid";
 import "./App.css";
 import FileUpload from "./FileUpload/FileUpload";
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-  const [files, setFiles] = useState([]);
-
-  const handleFileUpload = (files) => {
-    setFiles((prevFiles) => [...prevFiles, ...files]);
-  };
   return (
-    <div className="App"> 
-      <FileUpload onFileUpload={handleFileUpload} /> <FileGrid files={files} />
+    <div className="App">
+      <FileUpload /> <FileGrid />
     </div>
   );
 }
