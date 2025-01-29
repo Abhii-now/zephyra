853d09110643a2d612516d158aea06e47694e869
Abhinav Jain <jainabhinav2k16@gmail.com>
2025-01-29 20:59:01 +0530

    feat: Add feature for user details and create modal to share links based on view and download
    - Created ShareableLinkModal component to share links with view and download options.
    - Updated App.js to include the new modal component and handle its state.
    - Enhanced Redux slice to manage user details and shareable links.
    - Implemented functionality to fetch user details and generate shareable links.
    - Added error handling and validation for user selection and link type.

--- zephyra/src/index.js
+++ zephyra/src/index.js
@@ -12,25 +12,23 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <Provider store={store}>
-    <Router>
+  <Router>
+    <Provider store={store}>
       <Auth0Provider
         domain={process.env.REACT_APP_AUTH0_DOMAIN}
         clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
         authorizationParams={{
           redirect_uri: window.location.origin,
           audience: "http://127.0.0.1:8000/",
-          scope: "",
+          scope: "read:users read:current_user update:current_user_metadata",
+          prompt: "select_account",
         }}
       >
         <AuthWrapper />
       </Auth0Provider>
-    </Router>
-  </Provider>
+    </Provider>
+  </Router>
   // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
