86af4691f0cd3599652da3f10600437f968529a8
Abhinav Jain <jainabhinav2k16@gmail.com>
2025-01-29 20:59:28 +0530

    feat: Add feature for user details and create modal to share links based on view and download
    - Created ShareableLinkModal component to share links with view and download options.
    - Updated App.js to include the new modal component and handle its state.
    - Enhanced Redux slice to manage user details and shareable links.
    - Implemented functionality to fetch user details and generate shareable links.
    - Added error handling and validation for user selection and link type.

--- zephyra/src/features/auth/authSlice.js
+++ zephyra/src/features/auth/authSlice.js
@@ -2,13 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
   name: "auth",
-  initialState: { accessToken: "" },
+  initialState: { accessToken: "", userToken: "" },
   reducers: {
     setAccessToken: (state, action) => {
       state.accessToken = action.payload;
     },
+    setUserToken: (state, action) => {
+      state.userToken = action.payload;
+    },
   },
 });
 
-export const { setAccessToken } = authSlice.actions;
+export const { setAccessToken, setUserToken } = authSlice.actions;
 export default authSlice.reducer;
