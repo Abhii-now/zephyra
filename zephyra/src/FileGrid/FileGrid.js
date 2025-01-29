bf3950e4911124d88c45371d02f90b3d0f555f2b
Abhinav Jain <jainabhinav2k16@gmail.com>
2025-01-29 21:03:58 +0530

    feat: Add pages for download, upload, view, and grid view of files
    - Created FileDownload component for downloading files.
    - Created FileUpload component for uploading files.
    - Created FileView component for viewing files.
    - Created FileGrid component for displaying files in a grid view.
    - Set up routes in App.js to navigate to each of the new components.

--- zephyra/src/FileGrid/FileGrid.js
+++ zephyra/src/FileGrid/FileGrid.js
@@ -1,5 +1,5 @@
 // filepath: /Users/abhinavjain/dev/zephyra/zephyra/src/FileGrid.js
-import React from "react";
+import React, { useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
 import {
   AllEnterpriseModule,
@@ -7,7 +7,12 @@ import {
   themeAlpine,
 } from "ag-grid-enterprise";
 import { useDispatch, useSelector } from "react-redux";
-import { setSelectedFiles } from "../features/files/filesSlice";
+import { addFiles, setSelectedFiles } from "features/files/filesSlice";
+import customFetch from "utils/customFetch";
+import {
+  setError,
+  setShowAlert,
+} from "features/shareableLink/shareableLinkSlice";
 
 ModuleRegistry.registerModules([AllEnterpriseModule]);
 
@@ -15,15 +20,29 @@ const FileGrid = () => {
   const dispatch = useDispatch();
   const selectFiles = (state) => state.files;
   const files = useSelector(selectFiles).files;
-  console.log(files);
 
+  const fetchFiles = async () => {
+    try {
+      const response = await customFetch(
+        `${process.env.REACT_APP_API_BASE_URL}/mynewapp/files/`
+      );
+      if (response.ok) {
+        const files = await response.json();
+        console.log(files.files);
+        dispatch(addFiles(files.files));
+      } else {
+        throw new Error("Failed to fetch files");
+      }
+    } catch (error) {
+      dispatch(setError(error.message));
+      dispatch(setShowAlert(true));
+    }
+  };
+
+  useEffect(() => {
+    fetchFiles();
+  }, []);
   const columns = [
-    // {
-    //   headerCheckboxSelection: true,
-    //   checkboxSelection: true,
-    //   headerCheckboxSelectionFilteredOnly: true,
-    //   width: 50,
-    // },
     { headerName: "File Name", field: "name" },
     { headerName: "File Size (bytes)", field: "size" },
     { headerName: "Last Modified", field: "uploaded_at" },
@@ -37,7 +56,7 @@ const FileGrid = () => {
     <div
       className="ag-theme-alpine"
       data-testid="file-grid"
-      style={{ height: 400, width: "80%", margin: "auto" }}
+      style={{ height: 500, width: "80%", margin: "auto" }}
     >
       <AgGridReact
         theme={themeAlpine}
